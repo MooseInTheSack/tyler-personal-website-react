3de2e20fd78239dbf729a4c3fd680548dfd0a0ad
Tyler Kirkpatrick <tyl3rkirkpatrick@yahoo.com>
2021-05-11 17:58:10 -0500

    barebones structure with tabs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment }  from 'react';
 import './App.css';
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Container from '@material-ui/core/Container';
+import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
+import Contact from './components/Contact/Contact'
+import About from './components/About/About'
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <BrowserRouter>
+      <div className="App">
+      <Container maxWidth="md">
+
+        <Route
+          path="/"
+          render={({ location }) => (
+            <Fragment>
+              <Tabs centered="true" value={location.pathname}>
+                <Tab label="Home" component={Link} to="/" />
+                <Tab
+                  label="About"
+                  href="#basic-tabs"
+                  component={Link}
+                  to="/about"
+                />
+                <Tab label="Contact" component={Link} to="/contact" />
+              </Tabs>
+              <Switch>
+                <Route path="/contact" render={Contact} />
+                <Route path="/about" render={About} />
+                <Route path="/" render={Home} />
+              </Switch>
+            </Fragment>
+          )}
+        />
+        </Container>
+      </div>
+    </BrowserRouter>
+      
     </div>
   );
 }
