4007d72cbe67490023787a48960aa75c8b92610a
Tyler Kirkpatrick <tyl3rkirkpatrick@yahoo.com>
2021-06-28 12:29:32 -0600

    fleshed out home page, fixed tab issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment }  from 'react';
 import './App.css';
-import Tabs from "@material-ui/core/Tabs";
+import TabContext from '@material-ui/lab/TabContext';
+import TabList from '@material-ui/lab/TabList';
 import Tab from "@material-ui/core/Tab";
 import Container from '@material-ui/core/Container';
 import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
@@ -9,34 +10,73 @@ import Blog from './components/Blog/Blog'
 import Home from './components/Home/Home'
 
 function App() {
+
+  const [value, setValue] = React.useState('0');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  //                value={location.pathname}
+
   return (
     <div className="App">
     <BrowserRouter>
-      <Container disableGutters={true}>
-
-        <Route
-          path="/"
-          render={({ location }) => (
-            <Fragment>
-              <Tabs centered="true" value={location.pathname}>
-                <Tab label="Home" component={Link} to="/" />
-                <Tab
-                  label="Blog"
-                  href="#basic-tabs"
-                  component={Link}
-                  to="/blog"
-                />
-                <Tab label="Contact" component={Link} to="/contact" />
-              </Tabs>
-              <Switch>
-                <Route path="/contact" render={Contact} />
-                <Route path="/blog" render={Blog} />
-                <Route path="/" render={Home} />
-              </Switch>
-            </Fragment>
-          )}
-        />
-        </Container>
+      <TabContext value={value}>
+        
+          <Route
+            path="/"
+            render={({ location }) => (
+              <Fragment>
+                <TabList 
+                  centered="true" 
+                  onChange={handleChange}
+                >
+                  <Tab 
+                    label="Home" 
+                    component={Link} 
+                    to="/" 
+                    value="0"
+                  />
+                  <Tab 
+                    label="Projects" 
+                    component={Link} 
+                    to="/projects" 
+                    value="1"
+                  />
+                  <Tab
+                    label="Blog"
+                    href="#basic-tabs"
+                    component={Link}
+                    to="/blog"
+                    value="2"
+                  />
+                  <Tab 
+                    label="Contact" 
+                    component={Link} 
+                    to="/contact" 
+                    value="3"
+                  />
+                  <Tab 
+                    label="About" 
+                    component={Link} 
+                    to="/about" 
+                    value="4"
+                  />
+                </TabList>
+                <Container className="container" maxWidth="md">
+                  <Switch>
+                    <Route path="/contact" render={Contact} />
+                    <Route path="/blog" render={Blog} />
+                    <Route path="/" render={Home} />
+                    <Route path="/about" render={Home} />
+                    <Route path="/projects" render={Home} />
+                  </Switch>
+                </Container>
+              </Fragment>
+            )}
+          />
+      </TabContext>
     </BrowserRouter>
       
     </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,70 +2,95 @@ import React from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import SchoolIcon from '@material-ui/icons/School';
+import WorkIcon from '@material-ui/icons/Work';
+
+import selfie from '../pictures/red_shirt_selfie.png'
+
 
 import './Home.css';
+
+//    border: 1px solid black;
 
 export default function Home() {
   
   return (
     <div>
       <div className="Header">
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            Hello, my name is Tyler Kirkpatrick
+        <Grid className="Row" container spacing={3}>
+          <Grid item xs={12} className="gridBox">
+            <Typography variant="h3">Tyler Kirkpatrick</Typography>
+            <br />
+            <Typography variant="p">Doftware Developer. Runner. Computer Science & MBA Graduate.</Typography>
           </Grid>
         </Grid>
+
+        <Grid className="Row" container spacing={3}>
+          <Grid item xs={4}>
+            <Button variant="outlined" color="primary">Resume</Button>
+          </Grid>
+          <Grid item xs={4}>
+            <Button variant="outlined" color="default">Github</Button>
+          </Grid>
+          <Grid item xs={4}>
+            <Button variant="outlined" color="primary">LinkedIn</Button>
+          </Grid>
+        </Grid>
+          
+        
       </div>
+      
       <div className="Body">
-        <h2>Links</h2>
+        <Typography variant="h3">Career</Typography>
+        <br />
         <Grid className="Row" container spacing={3}>
-          <Grid item xs={4}>
-            <Paper className="Test">Resume</Paper>
+          <Grid item xs={3}>
+            <SchoolIcon fontSize="large" />
+            <br />
+            <Typography variant="p" className="title">Bachelor's Degree in Computer Science</Typography>
+            <br />
+            <Typography variant="p">Southern Methodist University</Typography>
+            
+            <Typography variant="subtitle2">Dallas, TX</Typography>
+            <Typography variant="subtitle2">August 2013 - May 2017</Typography>
+            <br />
           </Grid>
-          <Grid item xs={4}>
-            <Paper className="Test">Github</Paper>
-          </Grid>
-          <Grid item xs={4}>
-            <Paper className="Test">LinkedIn</Paper>
-          </Grid>
-        </Grid>
+          <Grid item xs={3}>
+            <WorkIcon fontSize="large" />
+            <br />
+            <Typography variant="p" className="title">Software Developer Intern</Typography>
+            <br />
+            <Typography variant="p">Anderson Merchandisers</Typography>
+            <br />
+            <Typography variant="subtitle2">Plano, TX</Typography>
+            <Typography variant="subtitle2">August 2017 - September 2017</Typography>
+            <br />
 
-        <h2>Strengths</h2>
-        <Grid className="Row" container spacing={3}>
-          <Grid item xs={4}>
-            <Paper className="Test">One</Paper>
           </Grid>
-          <Grid item xs={4}>
-            <Paper className="Test">Two</Paper>
+          <Grid item xs={3}>
+            <WorkIcon fontSize="large" />
+            <br />
+            <Typography variant="p" className="title">Software Developer III</Typography>
+            <br />
+            <Typography variant="p">Walmart</Typography>
+            <br />
+            <Typography variant="subtitle2">Bentonville, AR</Typography>
+            <Typography variant="subtitle2">November 2017 - July 2020</Typography>
+            <br />
           </Grid>
-          <Grid item xs={4}>
-            <Paper className="Test">Three</Paper>
-          </Grid>
-        </Grid>
-
-        <h2>Projects</h2>
-        <Grid className="Row" container spacing={3}>
-          <Grid item xs={4}>
-            <Paper className="Test">One</Paper>
-          </Grid>
-          <Grid item xs={4}>
-            <Paper className="Test">Two</Paper>
-          </Grid>
-          <Grid item xs={4}>
-            <Paper className="Test">Three</Paper>
-          </Grid>
-        </Grid>
-
-        <h2>Books</h2>
-        <Grid className="Row" container spacing={3}>
-          <Grid item xs={4}>
-            <Paper className="Test">Born to Run</Paper>
-          </Grid>
-          <Grid item xs={4}>
-            <Paper className="Test">The Organized Mind</Paper>
-          </Grid>
-          <Grid item xs={4}>
-            <Paper className="Test">Unbroken</Paper>
+          <Grid item xs={3}>
+            <SchoolIcon fontSize="large" />
+            <br />
+            <Typography variant="p" className="title">Masters in Business Administration</Typography>
+            <br />
+            <Typography variant="p">Texas A&M Mays Business School</Typography>
+            <br />
+            <Typography variant="subtitle2">College Station, TX</Typography>
+            <Typography variant="subtitle2">July 2020 - December 2021</Typography>
+            <br />
+            
           </Grid>
         </Grid>
       </div>
